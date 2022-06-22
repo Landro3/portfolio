f55aa9b5374cac6f33db272cb5cf4d29a790d580
Landro <andrew.landis3@gmail.com>
2022-06-22 10:51:27 -0400

    updated file download link

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,7 +2,7 @@ const Resume = () => {
   return (
     <div className='resume' id='resume'>
       <div>
-        <a className='resume-button' href='/' download>
+        <a className='resume-button' href='/Andrew_Landis_Resume.pdf' target='none' download>
           <i className='bi bi-arrow-down-circle' />
           &nbsp;
           Download Resume
